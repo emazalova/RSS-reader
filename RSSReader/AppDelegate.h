a51b84f61e71aeb8566487a981c4f0811a0e2efc
Katrin <brunetka__93@mail.ru>
2017-02-16 19:49:10 +0300

    first commit

---
+++ RSSReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSSReader
+//
+//  Created by Katrin on 16.02.17.
+//  Copyright © 2017 Katrin Mazalova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSSReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSSReader
+//
+//  Created by Katrin on 16.02.17.
+//  Copyright © 2017 Katrin Mazalova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
