01d14a7469b224950f95e944bb9306cdad7a8887
Katrin <brunetka__93@mail.ru>
2017-02-19 23:33:01 +0300

    save date for sort list

---
+++ RSSReader/NSDate+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+Extension.h
+//  RSSReader
+//
+//  Created by Katrin on 19.02.17.
+//  Copyright © 2017 Katrin Mazalova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Extension)
+
+@end
---
+++ RSSReader/NSDate+Extension.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+Extension.m
+//  RSSReader
+//
+//  Created by Katrin on 19.02.17.
+//  Copyright © 2017 Katrin Mazalova. All rights reserved.
+//
+
+#import "NSDate+Extension.h"
+
+@implementation NSDate (Extension)
+
+@end
